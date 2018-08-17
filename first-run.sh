a949b8d177c76eac413a0bfc3c389c70b35ebb06
Jere Karppinen <jere.karppinen@isolta.com>
2018-08-17 16:26:56 +0300

    Make another file for first run

--- first-run.sh
+++ first-run.sh
@@ -1,3 +1,6 @@
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install php -y
+pip3 install pytz
 php -S localhost:8080 -t /home/pi/busdisplay/php/ > /dev/null 2>&1 &
 export DISPLAY=:0
 python3 display.py > /dev/null 2>&1 &
