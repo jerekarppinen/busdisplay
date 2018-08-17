41e317b032937151962be28cabfb69514a286c64
Jere Karppinen <jere.karppinen@isolta.com>
2018-08-17 16:19:21 +0300

    Add php

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install php -y
 sudo apt-get install pytz
 php -S localhost:8080 -t /home/pi/busdisplay/php/ > /dev/null 2>&1 &
 export DISPLAY=:0
