5fe06a08c3ab8e0dfefa4c4e71548fa77d76dd44
Jere Karppinen <jere.karppinen@isolta.com>
2018-08-17 16:16:37 +0300

    Edit start

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install pytz
 php -S localhost:8080 -t /home/pi/busdisplay/php/ > /dev/null 2>&1 &
 export DISPLAY=:0
 python3 display.py > /dev/null 2>&1 &
