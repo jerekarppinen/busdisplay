cad27c42ce6967147ab346febf6a936ef8d2c9f0
Jere Karppinen <jere.karppinen88@gmail.com>
2019-03-13 13:09:29 +0200

    Remove unused config file

--- config.py
+++
@@ -1,3 +0,0 @@
-showStopAndDestination=0 # should be only used on wide screens
-waitUntilNextFullMinute=1 # this enables more accurate time keeping
-stopId=1491123 # place here your favourite line stop
