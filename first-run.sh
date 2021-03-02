fbeba8b0bc26d7fce006496ca3f34810e63bf2a0
Jere Karppinen <jere.karppinen88@gmail.com>
2021-03-02 08:27:58 +0200

    Update first-run.sh

--- first-run.sh
+++ first-run.sh
@@ -1,5 +1,6 @@
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install php -y
+pip3 install rpi-backlight
 pip3 install pytz
 php -S localhost:8080 -t /home/pi/busdisplay/php/ > /dev/null 2>&1 &
 export DISPLAY=:0
